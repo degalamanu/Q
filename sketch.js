e53f8a3931798ab6aa98b23ee0688b0a242ae724
degalamanu <79142917+degalamanu@users.noreply.github.com>
2021-06-02 14:49:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+  function setup(){
+    var PLAY;
+    var END ;
+  }
+  function bigFun(){
+    var a= document.getElementById("text1").value;
+    
+   
+              if(a=="hello"){
+                a=="0"
+  document.getElementById("messages").innerHTML="HI I AM AINOTE YOU CAN CALL ME NOTE";
+      return false;
+ }
+ if(a=="i am ill"){
+  a==""      
+  document.getElementById("messages").innerHTML="if you have fever type [!] if you have cold type[@] ";
+return false;
+
+
+    }
+    if(a=="@"){
+            
+      document.getElementById("messages").innerHTML="oh you have cold don't worry";
+          return false;
+    }
+    if(a=="!"){
+            
+      document.getElementById("messages").innerHTML="what is your body temperature?";
+          return false;
+    
+        }
+        if(a=>98){
+            
+          document.getElementById("messages").innerHTML="you have fever if you have headache please go to the doctor";
+              return false;
+        
+            }
+           
+            
+                
+            } 
